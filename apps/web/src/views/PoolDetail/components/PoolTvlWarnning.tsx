a5974fd8ef4d174fe812dfb243aef839cbd22062
chef-ryan <ryan@pancakeswap.com>
2024-10-31 10:52:46 +0800

    fix: 🐛 Fix text and ss/v2 links

--- apps/web/src/views/PoolDetail/components/PoolTvlWarnning.tsx
+++ apps/web/src/views/PoolDetail/components/PoolTvlWarnning.tsx
@@ -1,3 +1,5 @@
+import { getChainName } from '@pancakeswap/chains'
+import { Protocol } from '@pancakeswap/farms'
 import { useTranslation } from '@pancakeswap/localization'
 import { Message, MessageText, Text } from '@pancakeswap/uikit'
 import { PoolInfo } from 'state/farmsV4/state/type'
@@ -7,20 +9,24 @@ import { useRouterQuery } from '../hooks/useRouterQuery'
 const ONE_BILLION = 1_000_000_000
 export const PoolTvlWarning = ({ poolInfo }: { poolInfo: PoolInfo }) => {
   const { t } = useTranslation()
-  const { id } = useRouterQuery()
+  const { id, chainId } = useRouterQuery()
   const tvlUsd = parseFloat(poolInfo.tvlUsd ?? '0')
+  const { protocol } = poolInfo
+  const chain = getChainName(chainId)
+
+  const version = `${protocol === Protocol.V3 ? '/v3/' : ''}`
+  const stableSwap = `${protocol === Protocol.STABLE ? '?type=stableSwap' : ''}`
+  const link = `https://pancakeswap.finance/info/${version}${chain}/pairs/${id}${stableSwap}`
   if (tvlUsd < ONE_BILLION) {
     return null
   }
-  // const pairAddress = Pair.getAddress(token0.wrapped, token1.wrapped)
-  const link = `https://pancakeswap.finance/info/v3/pairs/${id}`
   return (
     <Message my="24px" mx="24px" variant="warning">
       <MessageText fontSize="17px">
         <Text color="warning" as="span">
           {t('TVL and APR data may not be reflected correctly due to technical limitation, please refer to')}{' '}
           <TextLink target="_blank" href={link}>
-            {t('this link')}
+            {t('info page')}
           </TextLink>{' '}
           {t('for accurate values')}
         </Text>
